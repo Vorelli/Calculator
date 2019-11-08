a584d5821d0244faff2779d87e708f557d5f3fb5
Natale Toscano <kickalliance195@gmail.com>
2019-11-07 22:56:38 -0500

    Add back button functionality and clean up the display

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,15 @@
 const buttons = document.querySelectorAll('#calculatorOutline div');
-const display = document.querySelector('#display');
+const display = document.querySelector('#display span');
+let divButton;
+const backButton = document.querySelector('#back');
 let toBeDeleted = [];
 let divsAndMults;
 let plusAndMinus;
 let numbers;
 for(let i = 0; i < buttons.length; i++){
     if('value' in buttons[i].dataset) {
+        if(buttons[i].dataset['value'] == '/')
+            divButton = buttons[i];
         if(buttons[i].getAttribute('data-value') == '=')
             buttons[i].addEventListener('click', parseDisplay)
         buttons[i].addEventListener('click', buttonClick);
@@ -13,23 +17,38 @@ for(let i = 0; i < buttons.length; i++){
     if(buttons[i].getAttribute('id') == 'clear')
         buttons[i].addEventListener('click', resetDisplay);
 }
+backButton.addEventListener('click',buttonClick);
+backButton.addEventListener('click',back);
+
+function back() {
+    display.textContent = display.textContent.substr(0, display.textContent.length-1);
+}
 
 function buttonClick() {
     if(this.getAttribute('data-value') != '=')
         display.textContent += this.getAttribute('data-value');
-    this.style.background = 'black';
-    this.style.color = 'white';
+    this.style.background = '#5F6368';
+    this.style.color = '#f5f5f5';
     setTimeout(resetColor, 100, this);
     setDisplayWidth();
 }
 
 function resetColor(button) {
-    button.style.background = 'white';
-    button.style.color = 'black';
+    button.style.background = '#f5f5f5';
+    button.style.color = '#5F6368';
 }
 
 function parseDisplay() {
     console.log(display.clientWidth)
+    refreshPositions();
+    
+    let lastNumIndex = 0;
+    for(let i = 0; i < numbers.length; i++){
+        if(display.textContent.indexOf(numbers[i], lastNumIndex) == -1){
+            display.textContent = display.textContent.substring(0,lastNumIndex) + '0' + display.textContent.substring(lastNumIndex,);
+        }
+        lastNumIndex += String(numbers[i]).length + 1;
+    }
     refreshPositions();
 
     while(divsAndMults.length>0) {
@@ -113,7 +132,7 @@ function findNumbers (){
                 displayError();
                 break;
             }
-            else if(/[.+-/*]/.test(display.textContent.charAt([i-1])))
+            else if(/[+-/*]/.test(display.textContent.charAt([i-1])))
                 displayError();
             else {
                 numEnd = i;
@@ -150,28 +169,43 @@ function displayError() {
 
 function resetDisplay() {
     display.textContent = '';
+    centerCalc();
 }
 
 function centerCalc() {
     document.querySelector('#calculatorOutline').firstElementChild.style.marginLeft = ((window.innerWidth-300)/2) + 'px';
+    setDisplayWidth();
+    backButton.style.position='absolute'
+    backButton.style.top='83px';
+    backButton.style.left = divButton.getBoundingClientRect().right+'px';
 }
 
 centerCalc();
+setDisplayWidth();
 window.onresize = centerCalc;
 
 
 function isOverflown(element) {
+    return element.clientWidth > 300;
     return element.scrollHeight > element.clientHeight || element.scrollWidth > element.clientWidth;
 }
 
 function setDisplayWidth() {
-    display.style.width = '300px';
+    display.parentElement.style.width = '300px';
     if(isOverflown(display)) {
-        display.style.width = '';
-        display.style.textAlign = 'center';
-        display.style.marginLeft = '';
+        display.parentElement.style.width = '';
+        display.parentElement.style.textAlign = 'center';
+        display.parentElement.style.marginLeft = '';
+        display.style.background = '#23232321';
+        display.parentElement.style.background = '';
+        display.style.position = 'absolute'
+        display.style.left = ((window.innerWidth-display.clientWidth)/2) + 'px';
     } else {
-        display.style.textAlign = 'left';
-        display.style.marginLeft = document.querySelector('#calculatorOutline').firstElementChild.style.marginLeft = ((window.innerWidth-300)/2) + 'px';
+        display.parentElement.style.textAlign = 'left';
+        display.style.left = '';
+        display.parentElement.style.background = '#23232321';
+        display.style.background = '';
+        display.style.position = 'absolute'
+        display.parentElement.style.marginLeft = document.querySelector('#calculatorOutline').firstElementChild.style.marginLeft = ((window.innerWidth-300)/2) + 'px';
     }
 }
